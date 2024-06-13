a8c992e8bbd7752f57f95b85b54c13905b685b82
calligrame <155944258+calligrame@users.noreply.github.com>
2024-06-13 17:34:15 +0900

    Add files via upload

---
+++ vol78/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>TS한국교통안전공단</title>
+ <meta http-equiv="Refresh" Content="0; URL=/page/vol77/index.html">
+ <meta property="og:image" content="/page/vol77/img/magazine_list/vol77.jpg"/>
+ <meta name="description" content="TS한국교통안전공단 웹진">
+</head> 
+<body>
+</body>
+</html>
